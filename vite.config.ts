f18b655c59163c4ac30fdaff55c792e0c53b2ec1
Laksanee P <laksanee.p@7solutions.co.th>
2025-08-31 07:17:45 +0700

    fix(deployment): fix deployment error

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.tsx?raw", "**/*.ts?raw"],
-  base: "/react-component-showcase/", // Replace with your repo name
+  base: "/react-components-showcase/",
   build: {
     outDir: "dist",
   },
